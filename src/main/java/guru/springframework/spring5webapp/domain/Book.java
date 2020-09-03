630df67a7e5869b122af9703548193cd0732c7d2
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-03 16:48:38 -0400

    Made POJO into Entities

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,9 +1,19 @@
 package guru.springframework.spring5webapp.domain;
 
+import sun.util.resources.Bundles;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Book
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private Set<Author> authors;
@@ -17,6 +27,16 @@ public class Book
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
     }
 
     public String getTitle()
